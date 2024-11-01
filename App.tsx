9026a16a94c792a3f2d2e7170343e46c9badc446
jox <zincir_890@hotmail.com>
2024-11-01 17:48:30 +0300

    Initial Commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback, useEffect } from 'react';
+import { Dimensions, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import Animated, { useAnimatedProps, useDerivedValue, useSharedValue, withTiming } from 'react-native-reanimated';
+import { ReText } from 'react-native-redash';
+import Svg, { Circle } from 'react-native-svg';
+
+const BACKGROUND_COLOR = '#444B6F';
+const BACKGROUND_STROKE_COLOR = '#303858';
+const STROKE_COLOR = '#A6E1FA';
+
+const { height, width } = Dimensions.get('window');
+
+const CIRCLE_LENGTH = 1000;
+const R = CIRCLE_LENGTH / (2 * Math.PI);
+
+const AnimatedCircle = Animated.createAnimatedComponent(Circle);
 
 export default function App() {
+
+  const progress = useSharedValue(0);
+
+  const animatedProps = useAnimatedProps(() => ({
+    strokeDashoffset: CIRCLE_LENGTH * (1 - progress.value)
+  }));
+
+  const progressText = useDerivedValue(() => {
+    return (
+      `${Math.floor(progress.value * 100)}`
+    )
+  });
+
+  const onPress = useCallback(() => {
+    progress.value = withTiming(progress.value > 0 ? 0 : 1, { duration: 2000 });
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ReText style={styles.progressText} text={progressText} />
+      <Svg style={{ position: 'absolute' }} fill={'none'}>
+        <Circle
+          cx={width / 2}
+          cy={height / 2}
+          r={R}
+          stroke={BACKGROUND_STROKE_COLOR}
+          strokeWidth={30}
+        />
+        <AnimatedCircle
+          cx={width / 2}
+          cy={height / 2}
+          r={R}
+          stroke={STROKE_COLOR}
+          strokeWidth={15}
+          strokeDasharray={CIRCLE_LENGTH}
+          strokeDashoffset={CIRCLE_LENGTH * 0.5}
+          animatedProps={animatedProps}
+          strokeLinecap={'round'}
+        />
+      </Svg>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={onPress}
+      >
+        <Text style={styles.buttonText}>Run</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +70,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: BACKGROUND_COLOR,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  progressText: {
+    fontSize: 80,
+    color: '#FFFFFF',
+    opacity: 0.7,
+    width: 200,
+    textAlign: 'center'
+  },
+  button: {
+    bottom: 80,
+    width: width * 0.7,
+    height: 60,
+    backgroundColor: BACKGROUND_STROKE_COLOR,
+    position: 'absolute',
+    borderRadius: 25,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    fontSize: 25,
+    color: 'white',
+    letterSpacing: 2.0
+  }
 });
